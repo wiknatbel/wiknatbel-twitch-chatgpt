812999bedb324152a2b4821191e71baeeab22db2
wiknatbel <133685510+wiknatbel@users.noreply.github.com>
2023-05-16 10:26:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/gpt/:text', async (req, res) => {
         model: "gpt-3.5-turbo",
         messages: messages,
         temperature: 0.5,
-        max_tokens: 128,
+        max_tokens: 399,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
