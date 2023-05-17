a75e86ca420709bb5a1bbb277dec3306a5a087b1
wiknatbel <133685510+wiknatbel@users.noreply.github.com>
2023-05-17 11:02:32 +0200

    index.js

--- index.js
+++ index.js
@@ -85,8 +85,49 @@ app.get('/gpt/:text', async (req, res) => {
         let agent_response = response.data.choices[0].message.content
 
         console.log ("Agent answer: " + agent_response)
-        messages.push({role: "assistant", content: "!" + agent_response})
+        messages.push({role: "assistant", content: agent_response})
 
         //Check for Twitch max. chat message length limit and slice if needed
         if(agent_response.length > 399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to
+          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+          agent_response = agent_response.substring(0, 399)
+          console.log ("Sliced agent answer: " + agent_response)
+        }
+
+        res.send(agent_response)
+      } else {
+        res.send("Something went wrong. Try again later!")
+      }
+
+    } else {
+      //PROMPT MODE EXECUTION
+      const prompt = file_context + "\n\nQ:" + text + "\nA:";
+      console.log("User Input: " + text)
+
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.5,
+        max_tokens: 128,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+      if (response.data.choices) {
+        let agent_response = response.data.choices[0].text
+          console.log ("Agent answer: " + agent_response)
+          //Check for Twitch max. chat message length limit and slice if needed
+          if(agent_response.length > 399){
+            console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+            agent_response = agent_response.substring(0, 399)
+            console.log ("Sliced Agent answer: " + agent_response)
+          }
+
+          res.send(agent_response)
+      } else {
+          res.send("Something went wrong. Try again later!")
+      }
+    }
+    
+})
+app.listen(process.env.PORT || 3000)
