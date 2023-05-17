2c0fd8414e4cb25cd7a9befc1346424a9228d488
wiknatbel <133685510+wiknatbel@users.noreply.github.com>
2023-05-17 14:53:40 +0200

    index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/gpt/:text', async (req, res) => {
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
-        timeout: 5,
+        timeout: 10,
       });
     
       if (response.data.choices) {
@@ -113,7 +113,7 @@ app.get('/gpt/:text', async (req, res) => {
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
-        timeout: 5,
+        timeout: 10,
       });
       if (response.data.choices) {
         let agent_response = response.data.choices[0].text
